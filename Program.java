2bc13144bd990db557ae86eb74804aa394ef5b93
SrikarVem <69221759+ScodingV@users.noreply.github.com>
2024-09-06 23:43:23 -0700

    Update Program.java
    Completed the tasks.

--- Program.java
+++ Program.java
@@ -1,9 +1,52 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
+public class NumberOperations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        // Input
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+
+        // Calculations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double average = (num1 + num2) / 2.0;
+        int absDifference = Math.abs(difference);
+
+        // Calculating Maximum and Minimum without using max or min functions
+        int maximum = (num1 + num2 + absDifference) / 2;
+        int minimum = (num1 + num2 - absDifference) / 2;
+
+        // Output
+        System.out.println("\nOriginal numbers are " + num1 + " and " + num2);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Average = " + average);
+        System.out.println("Absolute value = " + absDifference);
+        System.out.println("Maximum = " + maximum);
+        System.out.println("Minimum = " + minimum);
+        
+        input.close();
+    }
+}
 //Paste console output below:
 /*
+Enter number 1: 13
+Enter number 2: 20
 
+Original numbers are 13 and 20
+Sum = 33
+Difference = -7
+Product = 260
+Average = 16.5
+Absolute value = 7
+Maximum = 20
+Minimum = 13
 
 */
